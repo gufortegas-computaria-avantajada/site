4869c48c108522f6556c13da7d02ef35f6c4db12
gufortegas-computaria-avantajada <gufortegas@gmail.com>
2026-04-26 00:16:53 -0300

    script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const foruns = {
         geral: { nome: "Geral", imagem: "images.jpg" },
-        tecnologia: { nome: "Tecnologia", imagem: "images4.png" },
+        tecnologia: { nome: "Tecnologia", imagem: "images4.jpg" },
         teorias: { nome: "Teorias", imagem: "images2.png" },
         secreto: { nome: "???", imagem: "images3.jpg" },
         ultra: { nome: "ULTRA", imagem: "images3.jpg" }
