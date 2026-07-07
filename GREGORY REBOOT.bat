eb6c317370ff3bde472884132c4d7d0e2acb8ee9
gufortegas-computaria-avantajada <gufortegas@gmail.com>
2026-07-07 16:58:25 -0300

    Add files via upload

---
+++ GREGORY REBOOT.bat
@@ -0,0 +1,171 @@
+@echo off
+:chat loop
+title REBOOT
+color c
+ echo lembre se de nao usar acentos nem letras maiusculas 
+set /p "input=Fale com Gregory: "
+if /i "%input%"=="oi gregory" echo oi
+if /i "%input%"=="td bem" echo ss e tu
+if /i "%input%"=="tbm" echo otimo
+if /i "%input%"=="vc joga algum jg" echo ss
+if /i "%input%"=="qual" echo mentira sou um robo
+if /i "%input%"=="qual sua musica favorita?" echo tenho varias mais uma delas e master of puppets do metalica.
+if /i "%input%"=="opcoes" echo Digite /1 ate /60 para ver curiosidades. Tambem tenho 4 cifras. Para abrir as configuracoes digite .config
+REM ===========================
+REM      CURIOSIDADES
+REM ===========================
+
+if /i "%input%"=="/1" echo Girafas sobrevivem mais tempo sem agua do que camelos
+if /i "%input%"=="/2" echo O corpo humano tem mais de 96 mil km de vasos sanguineos
+if /i "%input%"=="/3" echo Estima-se que o Universo conte com mais de 10 bilhoes de galaxias
+if /i "%input%"=="/4" echo A luz do Sol leva cerca de 8 minutos e 20 segundos para chegar a Terra
+if /i "%input%"=="/5" echo O dia tem aproximadamente 23 horas e 56 minutos nao 24 horas
+if /i "%input%"=="/6" echo O cavalo marinho e o peixe mais lento que existe
+if /i "%input%"=="/7" echo Girafas dormem entre 10 minutos e 2 horas por dia
+if /i "%input%"=="/8" echo Em media cada pessoa perde 4 kg de pele morta em um ano
+if /i "%input%"=="/9" echo Existem mais formas de vida vivendo na sua pele do que humanos habitando a Terra
+if /i "%input%"=="/10" echo Ficar acordado por 2 semanas pode te matar
+if /i "%input%"=="/11" echo Charles Osborne teve uma crise de soluços que durou nada mais nada menos que 69 anos
+if /i "%input%"=="/12" echo Todas as pessoas que tem olhos azuis tem um mesmo ancestral em comum
+if /i "%input%"=="/13" echo Em suma a composicao quimica das lagrimas e diferente dependendo de seu motivo
+if /i "%input%"=="/14" echo Vacas conseguem subir escadas mas nao conseguem descer
+if /i "%input%"=="/15" echo Se ficarem de cabeca para baixo os tubaroes entram numa especie de coma
+if /i "%input%"=="/16" echo Um lapis pode escrever uma linha reta de ate 56 km
+if /i "%input%"=="/17" echo Ha mais de um seculo os cadernos de Marie Curie continuam radioativos
+if /i "%input%"=="/18" echo Somos povoados por bacterias
+if /i "%input%"=="/19" echo As pegadas dos astronautas ainda estao na Lua as bandeiras dos EUA tambem estao la
+if /i "%input%"=="/20" echo O cerebro de Einstein ainda esta entre nos
+if /i "%input%"=="/21" echo Somos o vazio
+if /i "%input%"=="/22" echo O Brasil e o pais que possui a maior comunidade japonesa fora do Japao So em Sao Paulo moram mais de 600 mil japoneses
+if /i "%input%"=="/23" echo O calendario da Etiopia e sete anos atrasado em relacao aos demais paises do ocidente
+if /i "%input%"=="/24" echo A avenida mais larga do mundo com 14 faixas diferentes fica na Argentina
+if /i "%input%"=="/25" echo Cerca de dois tercos dos habitantes da Terra nunca viram neve na vida
+if /i "%input%"=="/26" echo Existe uma cobertura para automoveis no topo do famoso Monte Everest
+if /i "%input%"=="/27" echo Existem mais de 200 corpos esquecidos de escaladores no Monte Everest
+if /i "%input%"=="/28" echo Ha mais bacterias na sua boca do que seres humanos no planeta
+if /i "%input%"=="/29" echo Os seres humanos tem dez vezes mais bacterias do que celulas
+if /i "%input%"=="/30" echo O coracao humano bate 100000 vezes por dia
+if /i "%input%"=="/31" echo Cerca de 30 por cento da superficie terrestre do planeta Terra e coberta por neve
+if /i "%input%"=="/32" echo Um rato pode viver mais sem agua do que um camelo
+if /i "%input%"=="/33" echo Aves nao poderiam ser enviadas ao espaco
+if /i "%input%"=="/34" echo Voce perde cerca de 4 kg de celulas da pele todos os anos
+if /i "%input%"=="/35" echo Uma pessoa tem em media 67 especies diferentes de bacterias em seu umbigo
+if /i "%input%"=="/36" echo Seu cerebro as vezes fica mais ativo quando voce esta dormindo do que quando esta acordado
+if /i "%input%"=="/37" echo Uma passa caida em um copo de champanhe fresco vai subir e descer continuamente do fundo do vidro ate o topo
+if /i "%input%"=="/38" echo Seu estomago precisa criar uma nova camada de muco a cada duas semanas ou ele digere ele mesmo
+if /i "%input%"=="/39" echo A maioria dos batons sao feitos com escamas de peixe
+if /i "%input%"=="/40" echo Nao ha relogios nos cassinos de Nova York
+if /i "%input%"=="/41" echo Cocos podem matar mais do que tubaroes
+if /i "%input%"=="/42" echo Nao existem formigas em alguns paises
+if /i "%input%"=="/43" echo Ratos nao vomitam
+if /i "%input%"=="/44" echo Os ursos sao canhotos
+if /i "%input%"=="/45" echo Voce pisca 25 mil vezes por dia
+if /i "%input%"=="/46" echo Os russos atendem ao telefone dizendo estou ouvindo
+if /i "%input%"=="/47" echo Ninguem consegue lamber o proprio cotovelo
+if /i "%input%"=="/48" echo O oceano Atlantico e mais salgado que o Pacifico
+if /i "%input%"=="/49" echo O elefante e o unico animal com 4 joelhos
+if /i "%input%"=="/50" echo A cada ano 98 por cento dos atomos sao substituidos
+if /i "%input%"=="/51" echo Rir durante o dia faz voce dormir melhor
+if /i "%input%"=="/52" echo 75 por cento das pessoas que leram a curiosidade 47 estao tentando lamber o cotovelo
+if /i "%input%"=="/53" echo No baralho o rei de copas e o unico que nao tem bigode
+if /i "%input%"=="/54" echo O som dos passos do ET no filme ET foi feito com gelatina
+if /i "%input%"=="/55" echo Uma galinha ja sobreviveu 18 meses sem a cabeca
+if /i "%input%"=="/56" echo Senhor Cabeca de Batata foi o primeiro brinquedo a ter uma propaganda de TV
+if /i "%input%"=="/57" echo Harry Houdini morreu no Halloween
+if /i "%input%"=="/58" echo Pogonofobia e o medo de barbas
+if /i "%input%"=="/59" echo Se voce gritar por 8 anos 7 meses e 6 dias produzira energia suficiente para uma xicara de cafe
+if /i "%input%"=="/60" echo Os egipcios acreditavam que os ouricos curavam a calvicie
+
+if /i "%input%"=="algo que goste" echo flores especificamente rosas
+if /i "%input%"=="fale uma musica" echo happy face,you are idiot,happy pills,choice,dark red,master of puppets,the final countdown
+if /i "%input%"=="cifra1" echo cifra de atbash (a-z) (b-y) (c-x) (d-w) (e-v) (f-u) (g-t) (h-s) (i-r) (j-q) (k-p) (l-o) (m-n) (n-m) (o-l) (p-k) (q-j) (r-i) (s-h) (t-g) (u-f) (v-e) (w-d) (x-c) (y-b) (z-a)
+if /i "%input%"=="fale em ingles" echo sorry I dont speak english
+if /i "%input%"=="um jogo que goste" echo nenhum sou uma maquina
+if /i "%input%"=="cifra2" echo cifra de cesar a=d b=e c=f d=g e=h f=i g=j h=k i=l j=m k=n l=o m=p n=q o=r p=s q=t r=u s=v t=w u=x v=y w=z x=a y=b z=c
+if /i "%input%"=="cifra3" echo cifra de albam a=i b=h c=g d=f e=n f=d g=c h=b i=a j=r k=p l=q m=o n=e o=m p=l q=k r=j s=z t=y u=x v=w w=v x=u y=t z=s
+if /i "%input%"=="cifra4" echo cifra de atbah a=n b=o c=p d=q e=r f=s g=t h=u i=v j=w k=x l=y m=z n=a o=b p=c q=d r=e s=f t=g u=h v=i w=j x=k y=l z=m
+if /i "%input%"=="^1" echo pointer pointer
+if /i "%input%"=="^2" echo emupedia
+if /i "%input%"==".config" echo mudar lingua para ingles digite ingles aviso ao trocar de lingua vc tera q escrever na lingua escolhida e tbm n fica permanente tem outras versoes tem outras cores na ingles e verde E TBM NA VERSAO DO INGLES AS CURIOSIDADES SAO+
+if /i "%input%"=="ingles" echo ok
+if /i "%input%"=="hi gregory" echo hello
+if /i "%input%"=="all well with you" echo yes and you
+if /i "%input%"=="too" echo nice
+if /i "%input%"=="do you play any game" echo no  
+REM ===========================
+REM      CURIOSIDADES EN-US
+REM ===========================
+if /i "%input%"=="options" echo Type /1 to /60 to see curiosities. I also have 4 ciphers. To open the settings type .settings
+
+if /i "%input%"=="+1" echo Giraffes survive longer without water than camels
+if /i "%input%"=="+2" echo The human body has over 96000 km of blood vessels
+if /i "%input%"=="+3" echo The Universe is estimated to have more than 10 billion galaxies
+if /i "%input%"=="+4" echo Sunlight takes about 8 minutes and 20 seconds to reach Earth
+if /i "%input%"=="+5" echo There are approximately 23 hours and 56 minutes in a day not 24 hours
+if /i "%input%"=="+6" echo The seahorse is the slowest fish ever
+if /i "%input%"=="+7" echo Giraffes sleep between 10 minutes and 2 hours a day
+if /i "%input%"=="+8" echo On average each person loses 4 kg of dead skin in a year
+if /i "%input%"=="+9" echo There are more life forms living on your skin than there are humans on Earth
+if /i "%input%"=="+10" echo Staying awake for 2 weeks can kill you
+if /i "%input%"=="+11" echo Charles Osborne had a bout of hiccups that lasted 69 years
+if /i "%input%"=="+12" echo All people with blue eyes share a common ancestor
+if /i "%input%"=="+13" echo The chemical composition of tears depends on why you are crying
+if /i "%input%"=="+14" echo Cows can climb stairs but cannot walk down them
+if /i "%input%"=="+15" echo If sharks are turned upside down they enter a kind of coma
+if /i "%input%"=="+16" echo A pencil can draw a straight line up to 56 km long
+if /i "%input%"=="+17" echo Marie Curies notebooks have remained radioactive for over a century
+
+REM ===========================
+REM      COMANDOS
+REM ===========================
+
+if /i "%input%"=="start calc" (
+    echo Ok.
+    start calc
+)
+
+if /i "%input%"=="start explorer" (
+    echo Ok.
+    start explorer
+)
+
+if /i "%input%"=="start chrome" (
+    echo Ok.
+    start chrome
+)
+
+if /i "%input%"=="start youtube" (
+    echo Ok.
+    start https://www.youtube.com/
+)
+
+if /i "%input%"=="r=nunca" (
+    echo Ok.
+    start "" "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+)
+
+if /i "%input%"=="toca master of puppets" (
+    echo Ok.
+    start "" "C:\Users\Gustavo\Music\Master Of Puppets.mp3"
+)
+
+if /i "%input%"=="start cmd" (
+    echo Ok.
+    start cmd
+)
+
+if /i "%input%"=="que horas sao" (
+    echo Consultando horario...
+    echo Agora sao %time:~0,5%
+)
+if /i "%input%"=="mostra minha playlist" (
+    echo Ok.
+    start "" "C:\Users\Gustavo\Music\"
+)
+if /i "%input%"=="toca minha playlist" (
+    echo Ok.
+    start winamp
+)
+goto chat loop 
+:end
+pause
